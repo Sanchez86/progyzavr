ff39c53b5dbdd59e2606dbec9af655f3e461f404
Aleksandr Koniev <0719118@gmail.com>
2024-04-01 21:51:23 +0300

    Update smallest_value_of_an_array.js

--- codewars7/smallest_value_of_an_array.js
+++ codewars7/smallest_value_of_an_array.js
@@ -1,13 +1,11 @@
-//
+// Smallest value of an array
 
 function min(arr, toReturn) {
-
-  const minValue = Math.min(...arr);
-  
 //   const numbers = arr.slice();
 //   const minValue = numbers.sort((a, b) => a-b)[0];
   
-  
+  const minValue = Math.min(...arr);  
   const indexMinValue = arr.indexOf(minValue);
   
   return toReturn === 'value' ? minValue : indexMinValue;
+}
