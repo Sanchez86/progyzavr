44bec13f1fe4841c465d3fc63491583cd4bda1b5
Aleksandr Koniev <0719118@gmail.com>
2024-04-14 21:51:58 +0300

    Create minMax.js

---
+++ codewars7/minMax.js
@@ -0,0 +1,23 @@
+// 
+
+function minMax(arr){
+  
+  let min = arr[0];
+  let max = arr[0];
+  
+  for(let i = 0; i<=arr.length; i++) {
+    if(arr[i] < min) min = arr[i];
+    if(arr[i] > max) max = arr[i];
+  }
+  
+  return [min, max];
+  
+//   const sorted = arr.sort((a,b) => a-b);
+  
+//   const min = sorted[0];
+//   const max = sorted[sorted.length-1];
+  
+//   return [min, max];
+  
+  // return [Math.min(...arr), Math.max(...arr)];
+}
