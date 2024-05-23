efe7c27326830b4fc8327c70fae09ac3ae2e532c
Aleksandr Koniev <0719118@gmail.com>
2024-05-23 22:54:12 +0300

    Create rndCode.js

---
+++ codewars7/rndCode.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5735956413c2054a680009ec/javascript
+
+function rndSymbols (arr, number) {
+  if (arr) {
+    const res = [];
+    const rnd = ~~(Math.random()*arr.length);
+    
+    for (let i = 0; i < number; i++) res.push(arr[rnd]);
+
+    return res.join('');
+  };
+  
+  if (number) {
+    const res = [];
+    
+    for (let i = 0; i < number; i++) res.push(~~(Math.random()*10));
+    
+    return res.join('');
+  }
+}
+
+function rndCode(){
+  const letters = "ABCDEFGHIJKLM";
+  const symbols = "~!@#$%^&*";
+  
+  return rndSymbols(letters, 2) + rndSymbols(null, 4) + rndSymbols(symbols, 2);
+}
+
+// ~~ === Math.floor
